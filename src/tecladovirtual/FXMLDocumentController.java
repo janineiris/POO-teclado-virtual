e82a9387a8e620b4f7de637a751ec6bd2b5684d7
Jonathan Bispo <jonathan26062001@hotmail.com>
2020-12-04 06:06:57 -0300

    adding audio interface

--- src/tecladovirtual/FXMLDocumentController.java
+++ src/tecladovirtual/FXMLDocumentController.java
@@ -6,29 +6,41 @@
 
 package tecladovirtual;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import tecladovirtual.sound.SoundController;
 
 /**
  *
  * @author iris
  */
 public class FXMLDocumentController implements Initializable {
+    private final SoundController C1 = new SoundController("src/tecladovirtual/sound/assets/1/C1.mp3");
+    private final SoundController D1 = new SoundController("src/tecladovirtual/sound/assets/1/D1.mp3");
     
     @FXML
-    private Label label;
+    private void actionBtnPlay(ActionEvent event) {
+        C1.play();
+    }
     @FXML
-    private Button button;
-    
+    private void actionBtnPlay1(ActionEvent event) {
+        D1.play();
+    }
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void actionBtnPause(ActionEvent event) {
+        C1.stop();
+        D1.stop();
     }
     
     @Override
---
+++ src/tecladovirtual/sound/SoundController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tecladovirtual.sound;
+
+import jaco.mp3.player.MP3Player;
+import java.io.File;       
+        
+/**
+ *
+ * @author JJSS
+ */
+public class SoundController implements SoundInterface{
+    private final MP3Player sound;
+
+    public SoundController(String path) {
+        this.sound = new MP3Player(new File(path));
+    }
+    
+    @Override
+    public void play(){
+        sound.play();
+    }
+    
+    @Override
+    public void stop(){
+        if(sound.isStopped() == false){
+            sound.stop();
+        }
+    }
+    
+}
